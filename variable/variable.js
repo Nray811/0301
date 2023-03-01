b832fea7744597a1be8051b8a1ee53434df45f22
Nray <ray08118686@gmail.com>
2023-03-01 15:17:48 +0800

    0k

---
+++ variable/variable.js
@@ -0,0 +1,16 @@
+var x = 1;
+let y = 2;
+const z = 3;
+
+{
+    var x = 'Java';
+    let y = 'css';
+    const z = 'html';
+    console.log(x);
+    console.log(y);
+    console.log(z);
+    console.log('.'.repeat(60));
+}
+console.log(x);
+console.log(y);
+console.log(z);
